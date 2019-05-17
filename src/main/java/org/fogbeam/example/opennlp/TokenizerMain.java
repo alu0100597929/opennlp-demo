fa61bd7fd3d32d22927d8034b74788738eb8106f
alu0100597929 <alu0100597929@ull.edu.es>
2019-05-17 21:36:01 +0100

    readFile and demo_tokens added

--- src/main/java/org/fogbeam/example/opennlp/TokenizerMain.java
+++ src/main/java/org/fogbeam/example/opennlp/TokenizerMain.java
@@ -2,9 +2,11 @@
 package org.fogbeam.example.opennlp;
 
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+//import java.io.FileInputStream;
+//import java.io.IOException;
+//import java.io.InputStream;
+import java.io.*;
+
 
 import opennlp.tools.tokenize.Tokenizer;
 import opennlp.tools.tokenize.TokenizerME;
@@ -30,11 +32,14 @@ public class TokenizerMain
 			Tokenizer tokenizer = new TokenizerME(model);
 			
 				/* note what happens with the "three depending on which model you use */
-			String[] tokens = tokenizer.tokenize
+			/*
+                        String[] tokens = tokenizer.tokenize
 					(  "A ranger journeying with Oglethorpe, founder of the Georgia Colony, " 
 							+ " mentions \"three Mounts raised by the Indians over three of their Great Kings" 
 							+ " who were killed in the Wars.\"" );
-			
+			*/
+                        String[] tokens = tokenizer.tokenize(readFile());
+                        
 			for( String token : tokens )
 			{
 				System.out.println( token );
@@ -60,4 +65,28 @@ public class TokenizerMain
 		}
 		System.out.println( "\n-----\ndone" );
 	}
+        
+        public static String readFile()
+        {
+            try
+            {
+                InputStream is = new FileInputStream("demo_tokens/tokens.txt");
+                BufferedReader bf = new BufferedReader(new InputStreamReader(is));
+
+                String line = bf.readLine();
+                StringBuilder sb = new StringBuilder();
+
+                while(line != null)
+                {
+                    sb.append(line).append("\n");
+                    line = bf.readLine();
+                }
+                String tokens = sb.toString();
+                return tokens;
+            }
+            catch(Exception e)
+            {
+                return null;
+            }  
+        }
 }
